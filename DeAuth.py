86837e018880d17c62663ab70a63e6dd82832a85
SquarePlayn <square.playn@gmail.com>
2019-03-15 12:18:44 +0100

    Fix imports

--- DeAuth.py
+++ DeAuth.py
@@ -1,8 +1,10 @@
-from scapy.all import *
+from scapy import *
+import sys
+from scapy.layers.dot11 import *
 
-count = sys.argv[1] #Amount of DeAuth's to send
-targetMAC = sys.argv[2] #Target MAC Address
-APMAC = sys.argv[3] #Access Point MAC Address
+count = sys.argv[1]  #Amount of DeAuth's to send
+targetMAC = sys.argv[2]  #Target MAC Address
+APMAC = sys.argv[3]  #Access Point MAC Address
 iface = sys.argv[4]
 
 #Constructing DeAuth message

